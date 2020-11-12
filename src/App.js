786b72072b626964af3c630ae0c4a071b74bfb16
chikechris <cchike2017@gmail.com>
2020-11-12 13:05:35 -0500

    applied styles, ste up use state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
-import React from 'react'
+import React, {useState} from 'react'
+import List  from './List'
+import data from './data'
 
 const App = () => {
+const [people, setPeople] = useState(data)
   return (
-    <div>
-      
-    </div>
+    <section className= 'container'>
+      <h3>We have {people.length} Birthdays Today</h3>
+      <List people = {people}/>
+      <button onClick={()=> setPeople([])}>Clear All</button>
+    </section>
   )
 }
 
